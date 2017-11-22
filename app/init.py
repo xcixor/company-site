bdf88a20200b9479131b6e299e4a101e0adca4e4
Peter <pndungu54@gmail.com>
2017-11-23 00:02:17 +0300

    create initial configuration

---
+++ app/init.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1> hello </h1>'
+
+def create_app():
+    return app
--- run.py
+++ run.py
@@ -1,10 +1,6 @@
-from flask import flask
+from app import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello Bernice"
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug = True)
