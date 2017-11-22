2d5af415a818e097a504ccca67b222f1e2cff3cc
Peter <pndungu54@gmail.com>
2017-11-23 00:51:30 +0300

    Refactor: modify how the app is being created

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+"""Creates and runs the app"""
+
 from app import create_app
 
-app = create_app()
+app = create_app('default')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
+    
