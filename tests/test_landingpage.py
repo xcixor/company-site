8797b89408491d2a487b1ac4e7f57ab3654c57d0
Peter <pndungu54@gmail.com>
2017-12-08 14:21:39 +0300

    Create the test suite

---
+++ tests/test_landingpage.py
@@ -0,0 +1,12 @@
+"""Tests the landing Blueprint"""
+
+import unittest
+
+from app import landing
+
+class TestLandingBlueprint(unittest.TestCase):
+    """Tests the landing module"""
+
+    def test_sampletest(self):
+        """Sample test"""
+        self.assertEqual(landing.views.sample_test(400), True)
