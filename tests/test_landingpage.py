e84c5f0fc3ab54c0bfed7a51bc60bd03fc2aa8b5
Peter <pndungu54@gmail.com>
2017-12-08 16:56:13 +0300

    Create test for landing page

--- tests/test_landingpage.py
+++ tests/test_landingpage.py
@@ -2,11 +2,26 @@
 
 import unittest
 
-from app import landing
+from app import create_app
 
 class TestLandingBlueprint(unittest.TestCase):
     """Tests the landing module"""
 
-    def test_sampletest(self):
-        """Sample test"""
-        self.assertEqual(landing.views.sample_test(400), True)
+    def setUp(self):
+        """Set up some stuff"""
+        #create app with a test client
+        self.app = create_app('testing').test_client()
+
+    def tearDown(self):
+        """Remove the stuff set up above"""
+        pass
+
+    def test_home_status_code(self):
+        """Test whether home is being rendered appropriately"""
+        # sends HTTP GET request to the application on the specified path
+        response = self.app.get('/')
+        # assert the response data
+        self.assertEqual(response.status_code, 200)
+        # useful tips on statuses at www.flaskapi.org
+             
+
