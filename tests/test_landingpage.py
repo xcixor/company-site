d24696c3a3fc7d6c3c4150fae0bf6a4470f6465f
Peter <pndungu54@gmail.com>
2017-12-08 17:14:23 +0300

    Fix: Rename home status response

--- tests/test_landingpage.py
+++ tests/test_landingpage.py
@@ -16,7 +16,7 @@ class TestLandingBlueprint(unittest.TestCase):
         """Remove the stuff set up above"""
         pass
 
-    def test_home_status_code(self):
+    def test_home_render_sucess(self):
         """Test whether home is being rendered appropriately"""
         # sends HTTP GET request to the application on the specified path
         response = self.app.get('/')
